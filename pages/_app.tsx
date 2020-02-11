8c59cc1afd75cdd56875c987137c5340895890c4
dherv <damien.herv@gmail.com>
2020-02-11 16:15:49 +0900

    chore(style): add global styles with normalize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Jiuji Theory</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Bungee+Shade|Poiret+One&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
