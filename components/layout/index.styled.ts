ed127c6cfc6fc7d29c7caca2c961928adbfbebda
dherv <damien.herv@gmail.com>
2020-03-28 10:53:24 +0900

    refactor(layout): add margin to html main tag

--- components/layout/index.styled.ts
+++ components/layout/index.styled.ts
@@ -4,6 +4,8 @@ export const SCLayout = {
   Layout: styled.div``,
   MainContainer: styled.div`
     width: 100%;
+  `,
+  Main: styled.main`
     margin: 2rem;
   `
 };
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -8,9 +8,9 @@ const Layout: FC<{}> = ({ children }) => {
       <header>
         <Nav />
       </header>
-      <main>
+      <SC.Main>
         <SC.MainContainer>{children}</SC.MainContainer>
-      </main>
+      </SC.Main>
     </SC.Layout>
   );
 };
